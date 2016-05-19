aea771e46613427d0984e127f490cc50035d30dc
Guanglei Wang <wgleiuni@gmail.com>
2016-05-19 10:46:22 -0700

    MTB.m: Dispersion relation of the three-band material

--- MTB.m
+++ MTB.m
@@ -1,9 +1,11 @@
 % Mechanical Topological Insulator, graphene and three-band material
-% First to reproduce Srep18107
+% Three band material
 
 %% Parameters
-eta=3/5;
-kappa=1/(1-eta/2);
+eta1=1;
+eta2=1;
+kappa1=1;
+kappa2=1/2;
 R1=[sqrt(3);1];
 R2=[-sqrt(3);1];
 R3=[0;-2];
@@ -18,9 +20,13 @@ a1=a1/norm(a1);
 a2=a2/norm(a2);
 
 %% Gamma matrix
-Gamma1=getGamma(R1,eta);
-Gamma2=getGamma(R2,eta);
-Gamma3=getGamma(R3,eta);
+Gamma1a=getGamma(R1,eta1);
+Gamma1b=getGamma(R2,eta1);
+Gamma1c=getGamma(R3,eta1);
+
+Gamma2a=getGamma(R1,eta2);
+Gamma2b=getGamma(R2,eta2);
+Gamma2c=getGamma(R3,eta2);
 
 %% Calculate band structure
 kx1=linspace(0,1,2000);
@@ -34,14 +40,15 @@ Ky=[ky1,ky2,ky3];
 K=[Kx',Ky']/(3/2/pi);
 
 numK=length(Kx);
-Ei=zeros(numK,4);
+Ei=zeros(numK,6);
 
 for i=1:numK
     k=K(i,:);
-    GammaAB1=-kappa*(Gamma3+exp(1i*k*a1)*Gamma1+exp(1i*k*a2)*Gamma2);
-    GammaAB2=-kappa*(Gamma3+exp(-1i*k*a1)*Gamma1+exp(-1i*k*a2)*Gamma2);
-    diag=3*kappa*(1-eta/2)*eye(2);
-    Gamma=[diag,GammaAB1;GammaAB2,diag];
+    GammaAB=-kappa1*(Gamma1c+exp(1i*k*a1)*Gamma1a+exp(1i*k*a2)*Gamma1b);
+    GammaBC=-kappa2*(Gamma2a+exp(-1i*k*(a1-a2))*Gamma2b+exp(-1i*k*a1)*Gamma2c);
+    diag1=3*kappa1*(1-eta1/2)*eye(2);
+    diag2=3*kappa2*(1-eta2/2)*eye(2);
+    Gamma=[diag1,GammaAB,zeros(2);GammaAB',diag1+diag2,GammaBC;zeros(2),GammaBC',diag2];
     Ei(i,:)=eig(Gamma);
 end
 
